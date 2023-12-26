2b36d95c457049b1089661f715083fc1d9bcd8e9
aeshapatel025 <aesha.patel@spreadd.io>
2023-12-26 10:13:59 +0530

    Add changes of css in avatar

--- src/Avatar/Avatar.Styled.js
+++ src/Avatar/Avatar.Styled.js
@@ -1,13 +1,12 @@
 import styled, { css } from 'styled-components';
 
 const sizes = {
-  small: { width: '3.4rem', height: '3.4rem' },
-  medium: { width: '4.4rem', height: '4.4rem' },
-  large: { width: '5.4rem', height: '5.4rem' },
+  small: { width: '34px', height: '34px' },
+  medium: { width: '44px', height: '44px' },
+  large: { width: '54px', height: '54px' },
 };
 
 const avatarStyles = css`
-  align-items: center;
   ${({ type }) => `
     width: ${sizes[type]?.width};
     height: ${sizes[type]?.height};
@@ -15,16 +14,8 @@ const avatarStyles = css`
 `;
 
 export const StyledAvatarComponent = styled.div`
-  display: flex;
-  flex-direction: row;
-  gap: 1rem;
-  justify-content: center;
-
   ${({ variant }) => css`
-    align-items: ${variant === 'square'
-        ? 'center; flex-wrap: wrap;'
-        : 'center;'}
-      img {
+    img {
       border-radius: ${variant === 'square' ? '0.5rem' : '50%'};
       display: block;
       overflow: hidden;
@@ -39,6 +30,7 @@ export const AvatarContainer = styled.div`
   flex-direction: row;
   gap: 1rem;
   justify-content: center;
+  align-items: center;
 `;
 
 export default StyledAvatarComponent;
