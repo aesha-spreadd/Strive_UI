5f4994e88e7c3db7bfd04407e7d272a8df6dd23a
aeshapatel025 <aesha.patel@spreadd.io>
2023-12-27 17:17:17 +0530

    Your commit message here

--- src/Badges/Badges.js
+++ src/Badges/Badges.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import { StyledBadgesComponent } from './Badges.Styled';
-import { count } from 'console';
 
-const Badges = ({ count, shape, img, count, color }) => {
+const Badges = ({ count, type, img, size, color }) => {
   return (
-    <StyledBadgesComponent
-      shape={shape}
-      count={count}
-      color={color}
-      size={size}
-    >
+    <StyledBadgesComponent type={type} count={count} color={color} size={size}>
       <div className="badge-content">
         <span className="badge-number">{count}</span>
         <img src={img} alt="badge" />
