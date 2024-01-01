380e549e10db3b5a5db370306e7b449feaece2da
aeshapatel025 <aesha.patel@spreadd.io>
2024-01-01 17:32:03 +0530

    add changes of input add prffix and suffix

--- src/Input/Input.js
+++ src/Input/Input.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { StyledInputComponent } from './Input.Styled';
+import { StyledInputComponent, Prefix, Suffix } from './Input.Styled';
 
-const Input = ({ name, placeholder }) => {
+const Input = ({ name, placeholder, suffix, prefix }) => {
   return (
     <StyledInputComponent name={name}>
+      {prefix && <Prefix>{prefix}</Prefix>}
       <input type="text" placeholder={placeholder} />
+      {suffix && <Suffix>{suffix}</Suffix>}
     </StyledInputComponent>
   );
 };
