9de5216370621398bb73cca0dd5fda29bf33194b
aeshapatel025 <aesha.patel@spreadd.io>
2024-01-02 08:33:36 +0530

    Remove Input folder

--- src/Typography/Typography.js
+++ src/Typography/Typography.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { StyledTypographyComponent } from './Typography.Styled.js';
 
-const Typography = ({ tag, text, typeColor }) => {
+const Typography = ({ tag, text, typeColor, style }) => {
   return (
-    <StyledTypographyComponent as={tag} typeColor={typeColor}>
+    <StyledTypographyComponent as={tag} typeColor={typeColor} style={style}>
       {text}
     </StyledTypographyComponent>
   );
