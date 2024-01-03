653ca1e1468768526264aba128fb66fc0534f48c
aeshapatel025 <aesha.patel@spreadd.io>
2024-01-03 15:00:56 +0530

    theme.js file

--- src/theme.js
+++ src/theme.js
@@ -3,12 +3,12 @@ const theme = {
   secondary: '#ff8242',
   success: '#07cb6e',
   warning: '#f3b664',
-  textLinkColors: '#000',
+  black: '#000',
   inputBorder: 'rgb(74, 74, 104)',
   textColor: 'rgb(246, 249, 252)',
   danger: 'rgb(244, 67, 54)',
   cardTextColor: 'rgb(74, 74, 104)',
-  pastelCardColor: '#E0AED0',
+  pastelColor: '#E0AED0',
 };
 
 export default theme;
